9faa01c2d4081ed1c402f1f799f77ff9607b0f17
miga-choi <miga.choi.1004@gmail.com>
2024-08-31 23:23:36 +0900

    add functions to HTML Parser

--- src/dom.rs
+++ src/dom.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
+
+
 /*
     The DOM is a tree of nodes. A node has zero or more children. (It also has
     various other attributes and methods, but we can ignore most of those for now.)
  */
-struct Node {
+
+pub struct Node {
     // data common to all nodes
     children: Vec<Node>,
 
@@ -11,33 +14,39 @@ struct Node {
     node_type: NodeType,
 }
 
+
 /*
     There are several node types, but for now we will ignore most of them and say
     that a node is either an Element or a Text node. In a language with inheritance
     these would be subtypes of Node. In Rust, they can be enum (Rust's keyword for
     a "tagged union" or "sum type").
  */
-enum NodeType {
+
+pub enum NodeType {
     Text(String),
     Element(ElementData),
 }
+
 
 /*
     An element includes a tag name and any number of attributes, which can be stored
     as a map from names to values. This engine doesn't support namespaces, so it just
     stores tag and attribute names as simple strings.
  */
-struct ElementData {
+
+pub struct ElementData {
     tag_name: String,
     attrs: AttrMap,
 }
 
-type AttrMap = HashMap<String, String>;
+pub type AttrMap = HashMap<String, String>;
+
 
 /*
     Some constructor functions to make it easy to create new codes:
  */
-fn text(data: String) -> Node {
+
+pub fn text(data: String) -> Node {
     Node {
         children: Vec::new(),
         node_type: NodeType::Text(data),
--- src/html.rs
+++ src/html.rs
@@ -1,3 +1,5 @@
+use crate::dom;
+
 /*
     HTML has its own unique parsing algorithm. Unlike parsers from most programming
     languages and file formats, the HTML parsing algorithm does not reject invalid input.
@@ -34,17 +36,22 @@
     Servo's CSS-Parser(https://github.com/servo/rust-cssparser) library.
     It has no real error handling; in most cases, it just aborts when faced
     with unexpected syntax.
+ */
 
+
+/*
     The parser stores its input string and a current position within the string.
     The position is the index of the next character we haven't processed yet.
  */
+
 struct Parser {
     pos: usize, // "usize" is an unsigned integer, similar to "size_t" in C
     input: String,
 }
 
+
 /*
-    We can use this to implement some simple methods fro peeking at the next
+    We can use this to implement some simple methods for peeking at the next
     characters in the input.
  */
 impl Parser {
@@ -66,5 +73,103 @@ impl Parser {
         } else {
             panic!("Expected {:?} at byte {} but it was not found", s, self.pos);
         }
+    }
+
+    // Return true if all input is consumed.
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+
+
+    /*
+        Rust strings are stored as UTF-8 byte arrays. To go to the next character,
+        we can't just advance by one byte.
+     */
+
+    // Return the current character, and advance self.pos to the next character.
+    fn consume_char(&mut self) -> char {
+        let c = self.next_char();
+        self.pos += c.len_utf8();
+        c
+    }
+
+
+    /*
+        Often we will want to consume a string of consecutive characters. The "consume_while"
+        method consumes characters that meet a given condition, and returns them as a string.
+        This method's argument is a function that takes a char and returns a bool.
+     */
+
+    // Consume characters until `test` returns false.
+    fn consume_while(&mut self, test: impl Fn(char) -> bool) -> String {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+
+    /*
+        We can use this to ignore a sequence of space characters, or to consume a string of
+        alphanumeric characters.
+     */
+
+    // Consume and discard zero or more whitespace characters.
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    // Parse a tag or attribute name.
+    fn parse_name(&mut self) -> String {
+        self.consume_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+
+
+    /*
+        Now we're ready to start parsing HTML. To parse a single node, we look at its
+        first character to see if it is an element or a text node.
+     */
+
+    // Parse a single node.
+    fn parse_node(&mut self) -> dom::Node {
+        if self.starts_with("<") {
+            self.parse_element()
+        } else {
+            self.parse_text()
+        }
+    }
+
+    /*
+        In our simplified version of HTML, a text node can contain any character except "<".
+     */
+
+    // Parse a text node.
+    fn parse_text(&mut self) -> dom::Node {
+        dom::text(self.consume_while(|c| c != '<'))
+    }
+
+    /*
+        An element is more complicated. It includes opening and closing tags, and between them
+        any number of child nodes.
+     */
+
+    // Parse a single element, including its open tag, contents, and closing tag.
+    fn parse_element(&mut self) -> dom::Node {
+        // Opening tag.
+        self.expect("<");
+        let tag_name = self.parse_name();
+        let attrs = self.parse_attributes();
+        self.expect(">");
+
+        // Contents.
+        let children = self.parse_node();
+
+        // Closing tag.
+        self.expect("</");
+        self.expect(&tag_name);
+        self.expect(">");
+
+        dom::elem(tag_name, attrs, children)
     }
 }
