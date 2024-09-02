1cc452cad37c45c701abebe9ca78b043f950c727
miga-choi <miga.choi.1004@gmail.com>
2024-09-02 23:38:31 +0900

    start parsing stylesheet

--- src/css.rs
+++ src/css.rs
@@ -2,9 +2,11 @@
     A CSS stylesheet is a series of rules.
  */
 use std::collections::btree_map::Values;
+use std::num::ParseIntError;
+use std::panic::resume_unwind;
 
-struct Stylesheet {
-    rules: Vec<Rule>,
+pub struct Stylesheet {
+    pub rules: Vec<Rule>,
 }
 
 
@@ -13,14 +15,15 @@ struct Stylesheet {
     followed by a series of declarations enclosed in braces.
  */
 
-struct Rule {
-    selectors: Vec<Selector>,
-    declarations: Vec<Declaration>,
+pub struct Rule {
+    pub selectors: Vec<Selector>,
+    pub declarations: Vec<Declaration>,
 }
 
 
 /*
-    A selector can be a simple selector, or it can be a chain of selectors joined by combinators.
+    A selector can be a [simple selector](https://www.w3.org/TR/CSS2/selector.html#selector-syntax),
+    or it can be a chain of selectors joined by combinators.
     This engine supports only simple selectors for now.
 
     In this engine, a simple selector can include a tag name, an ID prefixed by '#',
@@ -30,14 +33,14 @@ struct Rule {
     There are many other types of selector (especially in CSS3), but this will do for now.
  */
 
-enum Selector {
+pub enum Selector {
     Simple(SimpleSelector),
 }
 
-struct SimpleSelector {
-    tag_name: Option<String>,
-    id: Option<String>,
-    class: Vec<String>,
+pub struct SimpleSelector {
+    pub tag_name: Option<String>,
+    pub id: Option<String>,
+    pub class: Vec<String>,
 }
 
 
@@ -45,9 +48,9 @@ struct SimpleSelector {
     A declaration is just a name/value pair, separated by a colon and ending with a semicolon.
  */
 
-struct Declaration {
-    name: String,
-    value: Value,
+pub struct Declaration {
+    pub name: String,
+    pub value: Value,
 }
 
 
@@ -55,14 +58,14 @@ struct Declaration {
     This engine supports only a handful of CSS's many value types.
  */
 
-enum Value {
+pub enum Value {
     Keyword(String),
     Length(f32, Unit),
     ColorValue(Color),
     // insert more values here
 }
 
-enum Unit {
+pub enum Unit {
     Px,
     // insert more values here
 }
@@ -72,10 +75,49 @@ enum Unit {
     Rust note: u8 is an 8-bit unsigned integer, and f32 is a 32-bit float.
  */
 
-struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
+
+/*
+    CSS has a straightforward [grammar](https://www.w3.org/TR/CSS2/grammar.html),
+    making it easier to parse correctly than its quirky cousin HTML.
+    When a standards-compliant CSS parser encounters a [parse error](https://www.w3.org/TR/CSS2/syndata.html#parsing-errors),
+    it discards the unrecognized part of the stylesheet but still process the remaining portions.
+    This is useful because it allows stylesheets to include new syntax but still produce well-defined
+    output in older browsers.
+ */
+
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
+
+/// Parse a whole CSS stylesheet.
+pub fn parse(source: String) -> Stylesheet {
+    let mut parser = Parser { pos: 0, input: source };
+    Stylesheet { rules: parser.parse_rules() }
+}
+
+impl Parser {
+    /// Return true if all input is consumed.
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+
+    /// Read the current character without consuming it.
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+}
+
+
+fn valid_identifier_char(c: char) -> bool {
+    // TODO: Include U+00A0 and higher.
+    matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_')
+}
