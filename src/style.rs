2598559d7249dd1ad357b0895a49f757ad61d75b
miga-choi <miga.choi.1004@gmail.com>
2024-09-08 20:54:40 +0900

    define "Node with CSS" type

--- src/style.rs
+++ src/style.rs
@@ -1,1 +1,35 @@
 //! Code for applying CSS styles to the DOM.
+
+use crate::dom::Node;
+use std::collections::HashMap;
+
+
+/// Map from CSS property names to values.
+/*
+    e.g.
+        PropertyMap { "display": "block" }
+ */
+pub type PropertyMap = HashMap<String, String>;
+
+
+/// A node with associated style data.
+/*
+    e.g.
+        StyledNode {
+            node: Node,
+            specified_values: PropertyMap,
+            children: Vec<StyleNode<'a>>,
+ */
+pub struct StyleNode<'a> {
+    pub node: &'a Node,
+    pub specified_values: PropertyMap,
+    pub children: Vec<StyleNode<'a>>,
+}
+
+
+/// CSS's `display` property
+pub enum Display {
+    Inline, // display: inline
+    Block,  // display: block
+    None,   // display: none
+}
